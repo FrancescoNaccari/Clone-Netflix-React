771992ae52e193bdbac1f8ee736fe7e8ec7970b2
FrancescoNaccari <francesco.n92@gmail.com>
2024-12-07 16:55:04 +0100

    home V0.05

--- src/components/modal/MovieModal.jsx
+++ src/components/modal/MovieModal.jsx
@@ -1,4 +1,4 @@
-// src/components/MovieModal.js
+// src/components/modal/MovieModal.jsx
 import React, { useEffect, useState } from 'react';
 import { Modal, Button, Row, Col } from 'react-bootstrap';
 import moviesService from '../../services/moviesService';
@@ -15,7 +15,6 @@ const MovieModal = ({ show, handleClose, movie, genresMap }) => {
     if (movie) {
       loadMovieDetails(movie.id);
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [movie]);
 
   const loadMovieDetails = async (movieId) => {
@@ -44,7 +43,9 @@ const MovieModal = ({ show, handleClose, movie, genresMap }) => {
 
       setSelectedMovie(updatedMovie);
 
-      const filteredSimilarMovies = similar.results.filter((item) => item.poster_path).map(mapSimilarMovieData);
+      const filteredSimilarMovies = similar.results
+        .filter((item) => item.poster_path)
+        .map(mapSimilarMovieData);
       setSimilarMovies(filteredSimilarMovies);
       setDisplayedSimilarMovies(filteredSimilarMovies.slice(0, 9));
     } catch (error) {
@@ -63,16 +64,9 @@ const MovieModal = ({ show, handleClose, movie, genresMap }) => {
   });
 
   const getGenresString = (genreIds) => {
-    // Controlla se genresMap è definito
-    if (!genresMap) {
-      return '';
-    }
-  
-    return genreIds
-      .map((id) => genresMap[id] || 'Sconosciuto') // se non trova il genere, usa 'Sconosciuto'
-      .join(' • ');
+    if (!genresMap) return '';
+    return genreIds.map((id) => genresMap[id] || 'Sconosciuto').join(' • ');
   };
-  
 
   const updateDisplayedSimilarMovies = () => {
     setDisplayedSimilarMovies(showAllSimilarMovies ? similarMovies : similarMovies.slice(0, 9));
@@ -89,102 +83,103 @@ const MovieModal = ({ show, handleClose, movie, genresMap }) => {
 
   return (
     <Modal show={show} onHide={handleClose} size="lg" centered>
-      <Modal.Header closeButton>
-        <Modal.Title>{selectedMovie?.title}</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        {/* Trailer */}
-        {selectedMovie?.trailerUrl && (
-          <div className="trailer-container mb-4">
-         <iframe
-  src={selectedMovie.trailerUrl}
-  title="Trailer"
-  frameBorder="0"
-  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-  allowFullScreen
-  className="trailer-frame"
-  height="100%"
-  width="100%"
-></iframe>
-          </div>
-        )}
-
-        {/* Movie Details */}
-        <div className="movie-details mt-4 px-4">
-          <h2>{selectedMovie?.title}</h2>
-          <div className="movie-info-line">
-            <span>{new Date(selectedMovie?.release_date).getFullYear()}</span>
-            <span>{selectedMovie?.runtime ? `${selectedMovie.runtime} min` : 'ND'}</span>
-          </div>
-          <p className="movie-description">
-            {selectedMovie?.overview || 'Descrizione non disponibile.'}
-          </p>
-        </div>
-
-        {/* Similar Movies */}
-        <div className="similar-titles mt-4 px-4">
-          <h3>Altri titoli simili</h3>
-          <Row>
-            {displayedSimilarMovies.length > 0 ? (
-              displayedSimilarMovies.map((similarMovie) => (
-                <Col xs={12} sm={6} md={4} key={similarMovie.id} className="mb-4">
-                  <div
-                    className="similar-movie-item p-2 text-start"
-                    onClick={() => openSimilarMovie(similarMovie)}
-                    style={{ cursor: 'pointer' }}
-                  >
-                    <div className="poster-container">
-                      <img
-                        src={`https://image.tmdb.org/t/p/w500${similarMovie.poster_path}`}
-                        alt={similarMovie.title}
-                        className="poster-img img-fluid"
-                      />
-                      <div className="movie-details-overlay mt-1 d-flex justify-content-between pe-1">
-                        <span>{new Date(similarMovie.release_date).getFullYear()}</span>
-                        <span>{similarMovie.runtime !== 'ND' ? `${similarMovie.runtime} min` : 'ND'}</span>
-                      </div>
-                    </div>
-                    <p className="movie-title mt-2">{similarMovie.title}</p>
-                    <p className="movie-description">{similarMovie.overview}</p>
-                  </div>
-                </Col>
-              ))
-            ) : (
-              <p>Nessun titolo simile disponibile.</p>
+      {selectedMovie && (
+        <>
+          <Modal.Header closeButton>
+            <Modal.Title>{selectedMovie.title}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            {/* Trailer */}
+            {selectedMovie.trailerUrl && (
+              <div className="trailer-container mb-4">
+                <iframe
+                  src={selectedMovie.trailerUrl}
+                  title="Trailer"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                  className="trailer-frame"
+                  height="100%"
+                  width="100%"
+                ></iframe>
+              </div>
             )}
-          </Row>
-          {similarMovies.length > 9 && (
-            <Button variant="link" onClick={toggleShowAllSimilarMovies}>
-              {showAllSimilarMovies ? 'Mostra Meno' : 'Mostra Tutti'}
-            </Button>
-          )}
-        </div>
 
-        {/* Footer Info */}
-        <div className="modal-footer-info mt-4 mx-2 mb-2 pb-4 px-4">
-          <h4>Info su {selectedMovie?.title}</h4>
-          <p>
-            <strong>Regia:</strong> {selectedMovie?.director}
-          </p>
-          <p>
-            <strong>Cast:</strong> {selectedMovie?.cast}
-          </p>
-          <p>
-            <strong>Sceneggiatura:</strong> {selectedMovie?.screenplay}
-          </p>
-          <p>
-            <strong>Generi:</strong> {selectedMovie?.genres}
-          </p>
-          <p>
-            <strong>Caratteristiche:</strong> {selectedMovie?.features || 'Non disponibile'}
-          </p>
-          <p>
-            <strong>Classificazione per età:</strong>{' '}
-            <span className="age-classification">{selectedMovie?.ageRating || 'ND'}</span>{' '}
-            {selectedMovie?.ageRatingDescription || ''}
-          </p>
-        </div>
-      </Modal.Body>
+            {/* Movie Details */}
+            <div className="movie-details mt-4 px-4">
+              <h2>{selectedMovie.title}</h2>
+              <div className="movie-info-line">
+                <span>Anno {selectedMovie.release_date ? new Date(selectedMovie.release_date).getFullYear() : 'ND'}</span><br />
+                <span>{selectedMovie.runtime ? `${selectedMovie.runtime} min` : 'ND'}</span>
+              </div>
+              <p className="movie-description mt-1">
+                {selectedMovie.overview || 'Descrizione non disponibile.'}
+              </p>
+            </div>
+
+            {/* Similar Movies */}
+            <div className="similar-titles mt-4 px-4">
+              <h3>Altri titoli simili</h3>
+              <Row>
+                {displayedSimilarMovies.length > 0 ? (
+                  displayedSimilarMovies.map((similarMovie) => (
+                    <Col xs={12} sm={6} md={4} key={similarMovie.id} className="mb-4">
+                      <div
+                        className="similar-movie-item p-2 text-start"
+                        onClick={() => openSimilarMovie(similarMovie)}
+                        style={{ cursor: 'pointer' }}
+                      >
+                        <div className="poster-container">
+                          <img
+                            src={`https://image.tmdb.org/t/p/w500${similarMovie.poster_path}`}
+                            alt={similarMovie.title}
+                            className="poster-img img-fluid"
+                          />
+                          <div className="movie-details-overlay mt-1 d-flex justify-content-between pe-1">
+                            <span>{similarMovie.release_date !== 'ND' ? new Date(similarMovie.release_date).getFullYear() : 'ND'}</span> 
+                            <span>{similarMovie.runtime !== 'ND' ? `${similarMovie.runtime} min` : 'ND'}</span>
+                          </div>
+                        </div>
+                        <p className="movie-title mt-2">{similarMovie.title}</p>
+                        <p className="movie-description">{similarMovie.overview}</p>
+                      </div>
+                    </Col>
+                  ))
+                ) : (
+                  <p>Nessun titolo simile disponibile.</p>
+                )}
+              </Row>
+              {similarMovies.length > 9 && (
+                <Button variant="link" onClick={toggleShowAllSimilarMovies}>
+                  {showAllSimilarMovies ? 'Mostra Meno' : 'Mostra Tutti'}
+                </Button>
+              )}
+            </div>
+
+            {/* Footer Info */}
+            <div className="modal-footer-info mt-4 mx-2 mb-2 pb-4 px-4">
+              <h4>Info su {selectedMovie.title}</h4>
+              <p>
+                <strong>Regia:</strong> {selectedMovie.director}
+              </p>
+              <p>
+                <strong>Cast:</strong> {selectedMovie.cast}
+              </p>
+              <p>
+                <strong>Sceneggiatura:</strong> {selectedMovie.screenplay}
+              </p>
+              <p>
+                <strong>Generi:</strong> {selectedMovie.genres}
+              </p>
+              <p>
+                <strong>Classificazione per età:</strong>{' '}
+                <span className="age-classification">{selectedMovie.ageRating || 'ND'}</span>{' '}
+                {selectedMovie.ageRatingDescription || ''}
+              </p>
+            </div>
+          </Modal.Body>
+        </>
+      )}
     </Modal>
   );
 };
