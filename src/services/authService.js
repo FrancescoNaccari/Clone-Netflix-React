fdf3713782953fe9df8ec27f2c048228ca21f759
FrancescoNaccari <francesco.n92@gmail.com>
2024-12-09 11:38:12 +0100

    fix

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { jwtDecode } from 'jwt-decode';
 
-const API_BASE_URL = process.env.REACT_APP_API_BACK;
+const API_BASE_URL = '';
 
 class AuthService {
   static async register(data) {
